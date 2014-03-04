5d8a89279da9527a8f357f597cb19d7832f98219
Dirk Thomas <dthomas@osrfoundation.org>
2014-03-04 10:39:59 -0800

    Rebase from 'debian/groovy/python_qt_binding'

--- cmake/sip_configure.py
+++ cmake/sip_configure.py
@@ -1,10 +1,11 @@
 import os
+import re
 import sys
 from PyQt4 import pyqtconfig
 
 if len(sys.argv) != 8:
-    print 'usage: %s build-dir sip-file output_dir include_dirs libs lib_dirs ldflags' % (sys.argv[0])
-    exit(1)
+    print('usage: %s build-dir sip-file output_dir include_dirs libs lib_dirs ldflags' % sys.argv[0])
+    sys.exit(1)
 
 # The SIP build folder, the SIP file, the output directory, the include directories, the libraries, the library directories and the linker flags.
 build_dir, sip_file, output_dir, include_dirs, libs, lib_dirs, ldflags = sys.argv[1:]
@@ -28,22 +29,22 @@ except OSError:
 # Run SIP to generate the code.  Note that we tell SIP where to find the qt
 # module's specification files using the -I flag.
 os.system(' '.join([
-  config.sip_bin,
-  '-c', build_dir,
-  '-b', os.path.join(build_dir, build_file),
-  '-I', config.pyqt_sip_dir,
-  '-w',
-  qt_sip_flags,
-  sip_file
+    config.sip_bin,
+    '-c', build_dir,
+    '-b', os.path.join(build_dir, build_file),
+    '-I', config.pyqt_sip_dir,
+    '-w',
+    qt_sip_flags,
+    sip_file
 ]))
 
 # Create the Makefile.  The QtModuleMakefile class provided by the
 # pyqtconfig module takes care of all the extra preprocessor, compiler and
 # linker flags needed by the Qt library.
 makefile = pyqtconfig.QtGuiModuleMakefile(
-  dir=build_dir,
-  configuration=config,
-  build_file=build_file
+    dir=build_dir,
+    configuration=config,
+    build_file=build_file
 )
 
 # hack to override makefile behavior which always prepend -l to libraries which is wrong for absolute paths
@@ -56,17 +57,26 @@ def custom_platform_lib_function(self, clib, framework=0):
     return default_platform_lib_function(self, clib, framework)
 pyqtconfig.QtGuiModuleMakefile.platform_lib = custom_platform_lib_function
 
-for include_dir in include_dirs.split(' '):
+
+# split paths on whitespace
+# while dealing with whitespaces within the paths if they are escaped with backslashes
+def split_paths(paths):
+    paths = re.split('(?<=[^\\\\]) ', paths)
+    return paths
+
+for include_dir in split_paths(include_dirs):
+    include_dir = include_dir.replace('\\', '')
     makefile.extra_include_dirs.append(include_dir)
-for lib in libs.split(' '):
+for lib in split_paths(libs):
     makefile.extra_libs.append(lib)
-for lib_dir in lib_dirs.split(' '):
+for lib_dir in split_paths(lib_dirs):
+    lib_dir = lib_dir.replace('\\', '')
     makefile.extra_lib_dirs.append(lib_dir)
 for ldflag in ldflags.split('\\ '):
     makefile.LFLAGS.append(ldflag)
 
 # redirect location of generated library
-makefile._target = os.path.join(output_dir, makefile._target)
+makefile._target = '"%s"' % os.path.join(output_dir, makefile._target)
 
 # Generate the Makefile itself
 makefile.generate()
